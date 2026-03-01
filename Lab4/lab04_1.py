56fea7343b7b176af67f30b400e9e8db5a0e7862
Dukkkk <dulguunbatmunkh1210@gmail.com>
2026-03-01 16:49:57 +0800

    lab4

--- Lab4/lab04_1.py
+++ Lab4/lab04_1.py
@@ -1,11 +1,19 @@
-list = [3, 4 , 2, 1, 9]
+def count_sqrts(nums_list):
+    """
+    nums_list: a list
+    Assumes that nums_list only contains positive numbers and that there are no duplicates.
+    Returns how many elements in nums_list are exact squares of elements in the same list, including itself.
+    """
+    # Your code here
+    count = 0
+    j = 0
+    for num in nums_list:
+        for j in nums_list:
+            if num == j ** 2:
+                count += 1
+                break 
+    return count
 
-def count_sqrts(list):
-    counts = {}
-    for num in list:
-        sqrt_num = num ** 0.5
-        if sqrt_num.is_integer():
-            sqrt_int = int(sqrt_num)
-            counts[sqrt_int] = counts.get(sqrt_int, 0) + 1
-    return counts
 
+# Examples:
+print(count_sqrts([3, 4, 2, 1, 9, 25]))  # prints 3
